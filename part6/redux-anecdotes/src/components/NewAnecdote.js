8a6fd7fe6b80032352b10b1c7ba2935fdf70d7d7
atulpat3l <patel.ap787@gmail.com>
2022-05-28 16:09:52 +0530

    Implemented voting functionality to anecdotes

---
+++ part6/redux-anecdotes/src/components/NewAnecdote.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const NewNote = () => {
+  return (
+    <>
+      <h2>create new</h2>
+      <form>
+        <div>
+          <input />
+        </div>
+        <button>create</button>
+      </form>
+    </>
+  );
+};
+
+export default NewNote;
